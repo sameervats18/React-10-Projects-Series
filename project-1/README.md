5e918ed25389496cff069092fcefb5eb927a0c9b
Sameervats18 <sameervatssingh@gmail.com>
2024-10-12 22:22:41 +0530

    okay

---
+++ project-1/README.md
@@ -0,0 +1,5 @@
+# React Project 1
+A Brand Page
+This is my project no. 1 which i have created .
+
+<img src="project-1 img.png" alt="A Brand Page">
---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+const HeroSection = () => {
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP WITH OUR SHOES.
+                    YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Category</button>
+                </div>
+                <div className="shopping">
+                    <p>Also available on</p>
+                    <div className="brand-icons">
+                        <img src="/public/images/bag.png" alt="amaz-logo" />
+                        <img src="/public/images/bag.png" alt="flip-logo" />
+                    </div>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="/public/images/shoes block.png" alt="hero-image" />
+            </div>
+        </main>
+    );
+};
+
+export default HeroSection;
