2602e5d724f23a78cd303db3b243ba4d946aa5a2
Sameervats18 <sameervatssingh@gmail.com>
2024-10-13 21:03:09 +0530

    Navigation

---
+++ project-2/src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="/images/Frame 2 1.png" alt="logo" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
