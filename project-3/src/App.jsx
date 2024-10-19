067b0c1ce9c6538038c74ec494d0b298dec4a812
Sameervats18 <sameervatssingh@gmail.com>
2024-10-19 23:27:25 +0530

    inProgress

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Krenge chaalu</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <code>src/App.jsx</code>
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite
       </p>
     </>
   )
