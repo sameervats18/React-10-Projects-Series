ad0a71372162fecc9675d51c1c9036f8d9734c1b
Sameervats18 <sameervatssingh@gmail.com>
2024-10-26 23:58:25 +0530

    inprog

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Krenge chaalu kuch samay ruk jaiye</h1>
+      <h1>Krenge chaalu kuch samay ruk jaiye ji</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
