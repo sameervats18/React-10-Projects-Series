33be889eefc0e1d8b04f5f36b259698d976dcf30
Sameervats18 <sameervatssingh@gmail.com>
2025-03-24 00:03:22 +0530

    final

--- project-6/src/main.jsx
+++ project-6/src/main.jsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
+import { theme } from "./theme/index.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ChakraProvider>
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
