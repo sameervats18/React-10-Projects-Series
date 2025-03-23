3fff4c93e3aafc0b1e682658509b80832e8e8d24
Sameervats18 <sameervatssingh@gmail.com>
2025-03-24 00:09:53 +0530

    completed

---
+++ project-7/README.md
@@ -0,0 +1,3 @@
+# project-7
+
+<img src="Final_Page.png" alt="Dashboard Authentication">
--- project-7/app/src/pages/Dashboard/Dashboard.jsx
+++ project-7/app/src/pages/Dashboard/Dashboard.jsx
@@ -4,8 +4,17 @@ import PortfolioSection from "./components/PortfolioSection";
 import PriceSection from "./components/PriceSection";
 import Transactions from "./components/Transactions";
 import InfoCard from "./components/InfoCard";
+import { fetchExample } from "../../api/query/exampleQuery";
+import { useQuery } from "react-query";
 
 const Dashboard = ({}) => {
+  const exampleQuery = useQuery({
+    queryKey: ["example"],
+    queryFn: fetchExample,
+  });
+
+  if (exampleQuery.isLoading) return <div>loading.....</div>;
+
   return (
     <DashboardLayout title="Dashboard">
       <Grid
